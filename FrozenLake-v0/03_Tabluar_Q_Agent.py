782e89a5e68d04ad898b4d2ab79f605f443df606
hyunjun529 <hyunjun529@gmail.com>
2017-03-06 10:50:44 +0900

    03 try 1

--- FrozenLake-v0/03_Tabluar_Q_Agent.py
+++ FrozenLake-v0/03_Tabluar_Q_Agent.py
@@ -2,15 +2,20 @@
 import argparse
 import logging
 import sys
+import numpy as np
 
 import gym
 from gym import wrappers
 
 
-class RandomAgent(object):
+class TabularQAgent(object):
     """The world's simplest agent!"""
-    def __init__(self, action_space):
+    def __init__(self, observation_space, action_space):
+        self.observation_space = observation_space
         self.action_space = action_space
+        self.Q = np.zeros([self.observation_space.n, self.action_space.n])
+        self.learning_rate = 0.85
+        self.dis = 0.99
 
     def act(self, observation, reward, done):
         return self.action_space.sample()
@@ -41,26 +46,32 @@ if __name__ == '__main__':
     # directory, including one with existing data -- all monitor files
     # will be namespaced). You can also dump to a tempdir if you'd
     # like: tempfile.mkdtemp().
-    outdir = './log/02'
+    outdir = './log/03'
     env = wrappers.Monitor(env, directory=outdir, force=True)
     env.seed(0)
-    agent = RandomAgent(env.action_space)
+    agent = TabularQAgent(env.observation_space, env.action_space)
 
     # h529 : 총 실행되는 epsiod의 수
-    episode_count = 100
+    episode_count = 10000
     reward = 0
     done = False
 
+    rList = []
     for i in range(episode_count):
         ob = env.reset()
-        while True:
-            action = agent.act(ob, reward, done)
-            ob, reward, done, _ = env.step(action)
-            if done:
-                break
-            # Note there's no env.render() here. But the environment still can open window and
-            # render if asked by env.monitor: it calls env.render('rgb_array') to record video.
-            # Video is not recorded every episode, see capped_cubic_video_schedule for details.
+        rAll = 0
+        done = False
+        while not done:
+            action = np.argmax(agent.Q[ob, :] + np.random.randn(1, env.action_space.n) / (i + 1))
+            ob2, reward, done, _ = env.step(action)
+            agent.Q[ob, action] = reward + agent.dis * np.max(agent.Q[ob2, :])
+            ob = ob2
+            rAll += reward
+        rList.append(rAll)
+
+    logger.info("Score over time" + str(sum(rList) / episode_count))
+    logger.info("Final Q-Table Values")
+    logger.info(agent.Q)
 
     # Close the env and write monitor result info to disk
     env.close()
