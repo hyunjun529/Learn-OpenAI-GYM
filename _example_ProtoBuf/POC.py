551e4d8aac5556e398ce0d2d16d07d49d132a550
hyunjun529 <hyunjun529@gmail.com>
2017-03-13 16:43:55 +0900

    hmm... not bad but...

--- _example_ProtoBuf/POC.py
+++ _example_ProtoBuf/POC.py
@@ -1,6 +1,8 @@
 import req_pb2
 import sys
 
+# write
+"""
 target = req_pb2.req()
 
 target.query = 'index'
@@ -10,14 +12,15 @@ target.cate = 666
 
 with open('data2', "wb") as f:
     f.write(target.SerializeToString())
+"""
 
 # read
 reader = req_pb2.req()
 
-with open('data2', "rb") as f:
+with open('data', "rb") as f:
     reader.ParseFromString(f.read())
 
 print(reader.query)
 print(reader.page)
 print(reader.rate)
-print(target.cate)
+print(reader.cate)
