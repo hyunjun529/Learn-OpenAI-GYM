ea2a64cb8323a46fdcaa63637d9ed46c39bf89f3
hyunjun529-desktop <hyunjun529@gmail.com>
2017-03-25 15:09:39 +0900

    get reacher log

--- Reacher-PyBullet/00_Random_Gym.py
+++ Reacher-PyBullet/00_Random_Gym.py
@@ -1,20 +1,34 @@
 import gym
+from gym import wrappers
+
 env = gym.make('Reacher-v1')
 env.reset()
 env.render()
 
-env.monitor.start('/tmp/reacher-1', force=True)
-for i_episode in xrange(101):
+outdir = './log/'
+
+f_act = open(outdir + 'log_act.txt', 'w')
+f_obs = open(outdir + 'log_obs.txt', 'w')
+f_rwd = open(outdir + 'log_rwd.txt', 'w')
+f_info = open(outdir + 'log_info.txt', 'w')
+
+env = wrappers.Monitor(env, directory=outdir, force=True)
+for i_episode in range(101):
     observation = env.reset()
-    for t in xrange(100):
+    for t in range(100):
         env.render()
-        # print observation
 
         # action selection
         action = env.action_space.sample()
 
         # take the action and observe the reward and next state
         observation, reward, done, info = env.step(action)
+
+        # print observation
+        f_act.write(str(action) + "\n")
+        f_obs.write(str(observation) + "\n")
+        f_rwd.write(str(reward) + "\n")
+        f_info.write(str(info) + "\n")
 
         if done:
             print("Episode finished after {} timesteps".format(t+1))
